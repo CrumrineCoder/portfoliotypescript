d6bab3c78f9601ee35af61e9df7c81de06211cfb
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-03-11 13:18:37 -0500

    setup copilot, working on figuring out the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import emailIcon from "./assets/email.svg";
+import githubIcon from "./assets/github.svg";
+import linkedinIcon from "./assets/linkedin.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Nicolas Crumrine
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="header-content">
+          <h1>Nicolas Crumrine</h1>
+          <div className="social-links">
+            <a
+              href="https://github.com/nicolas-crumrine"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={emailIcon} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nicolas-crumrine"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={emailIcon} />
+            </a>
+            <a
+              href="https://twitter.com/nicolas_crumrine"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={emailIcon} />
+            </a>
+          </div>
+        </div>
       </header>
     </div>
   );
