34f086361c51aa8f4bbec235bae1035852e5aab7
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-03-12 15:32:37 -0500

    header is now it's own component, adding old videos

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import "./App.scss";
 import React from "react";
 import Header from "./components/header";
 
+import NPC from "./assets/videos/NPC.mp4";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <video src={NPC} autoPlay loop muted />
     </div>
   );
 }
