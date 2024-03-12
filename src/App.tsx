289c8a9795b7d417d3b0155d2428800ae0b1aa84
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-03-12 15:10:35 -0500

    Header work

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,12 @@
 import "./App.scss";
 
 import React from "react";
-
-import emailIcon from "./assets/email.svg";
-import githubIcon from "./assets/github.svg";
-import linkedinIcon from "./assets/linkedin.svg";
-import profile from "./assets/pfp.jpg";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="left-elements">
-          <h1>Nicolas Crumrine</h1>
-          <img
-            src={profile}
-            className="profile-picture"
-            alt="Nicolas Crumrine's profile"
-          />
-        </div>
-        <div className="right-elements">
-          <div className="social-links">
-            <a
-              href="https://github.com/CrumrineCoder"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={githubIcon} alt="" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/nicolas-crumrine-50899b120/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedinIcon} alt="" />
-            </a>
-            <a
-              href="mailto:crumrinecoding@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={emailIcon} alt="" />
-            </a>
-          </div>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,42 @@
-// Header.tsx
-
 import React from 'react';
+import emailIcon from "../assets/email.svg";
+import githubIcon from "../assets/github.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+import profile from "../assets/pfp.jpg";
 
-interface HeaderProps {
-  name: string;
-  pictureUrl: string;
-  socialLinks: {
-    [key: string]: string;
-  };
-}
-
-const Header: React.FC<HeaderProps> = ({ name, pictureUrl, socialLinks }) => {
+const Header: React.FC = () => {
   return (
-    <header>
-      <div className="left">
-        <img src={pictureUrl} alt={`Profile of ${name}`} />
-        <h1>{name}</h1>
+    <header className="App-header">
+      <div className="left-elements">
+        <img
+          src={profile}
+          className="profile-picture"
+          alt="Nicolas Crumrine's profile"
+        />
+        <p>Nicolas Crumrine</p>
       </div>
-      <div className="right">
-        <ul>
-          {Object.entries(socialLinks).map(([platform, link]) => (
-            <li key={platform}>
-              <a href={link} target="_blank" rel="noopener noreferrer">
-                {platform}
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className="right-elements">
+        <a
+          href="https://github.com/CrumrineCoder"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubIcon} alt="" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/nicolas-crumrine-50899b120/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinIcon} alt="" />
+        </a>
+        <a
+          href="mailto:crumrinecoding@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={emailIcon} alt="" />
+        </a>
       </div>
     </header>
   );
