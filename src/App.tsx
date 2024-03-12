c499d2e5b4e1fe18c5deb3ffc930e263e3b85977
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-03-12 13:23:38 -0500

    working on header css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import "./App.scss";
 
 import React from "react";
-import logo from "./logo.svg";
-
 
 import emailIcon from "./assets/email.svg";
 import githubIcon from "./assets/github.svg";
 import linkedinIcon from "./assets/linkedin.svg";
-import profile from "./assets/pfp.jpg"
+import profile from "./assets/pfp.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="header-content">
-          <h1>Nicolas Crumrine</h1> 
-          <img src={profile} alt="Profile Picture" className="profile-picture" />
+        <div className="left-elements">
+          <h1>Nicolas Crumrine</h1>
+          <img
+            src={profile}
+            className="profile-picture"
+            alt="Nicolas Crumrine's profile"
+          />
+        </div>
+        <div className="right-elements">
           <div className="social-links">
             <a
               href="https://github.com/CrumrineCoder"
@@ -31,7 +34,7 @@ function App() {
               rel="noopener noreferrer"
             >
               <img src={linkedinIcon} alt="" />
-            </a>   
+            </a>
             <a
               href="mailto:crumrinecoding@gmail.com"
               target="_blank"
